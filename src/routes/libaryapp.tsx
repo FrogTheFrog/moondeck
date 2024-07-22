7c1c1c14ed5701264126ba262c3173559b99ea2c
FrogTheFrog <warliukz@gmail.com>
2024-07-22 14:48:28 +0300

    Update library app patching

--- src/routes/libaryapp.tsx
+++ src/routes/libaryapp.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { MoonDeckAppLauncher, SettingsManager, logger } from "../lib";
-import { Navigation, afterPatch, appDetailsClasses, findInReactTree, wrapReactType } from "@decky/ui";
+import { Navigation, afterPatch, appDetailsClasses, createReactTreePatcher, findInReactTree } from "@decky/ui";
 import { RoutePatch, routerHook } from "@decky/api";
 import { MoonDeckLaunchButtonAnchor } from "../components/moondecklaunchbutton";
 import { ReactElement } from "react";
@@ -9,100 +13,91 @@ interface Props {
   settingsManager: SettingsManager;
 }
 
-type FirstElement = ReactElement<
-{
-  children?: ReactElement<{
-    overview: {
-      app_type?: number;
-      appid?: number;
-      display_name?: string;
-    } | undefined;
-  } | undefined>;
-} | undefined>;
-
 function patchLibraryApp(route: string, { moonDeckAppLauncher, settingsManager }: Props): RoutePatch {
   return routerHook.addPatch(
     route,
-    (routeProps?: { path?: string; children?: ReactElement<{ renderFunc: boolean }> }) => {
-      if (!routeProps?.children?.props?.renderFunc) {
-        return routeProps;
-      }
+    (tree: any) => {
+      const routeProps = findInReactTree(tree, (x: any) => x?.renderFunc);
+      if (routeProps) {
+        let appType: number | undefined;
+        let appId: number | undefined;
+        let appName: string | undefined;
 
-      // No need for wrap
-      afterPatch(
-        routeProps.children.props,
-        "renderFunc",
-        (_1: Array<Record<string, unknown>>, ret1?: FirstElement): FirstElement | undefined | null => {
-          const ret1Children = ret1?.props?.children;
-          if (typeof ret1Children !== "object") {
-            logger.debug(`Failed to patch ${route}, ret1#children!`);
-            return ret1;
-          }
-
-          const overview = ret1Children.props?.overview;
-          if (typeof overview !== "object") {
-            logger.error(`Failed to patch ${route}, ret1#overview!`);
-            return ret1;
-          }
-
-          if (typeof overview.app_type !== "number") {
-            logger.error(`Failed to patch ${route}, ret1#app_type!`);
-            return ret1;
-          }
-
-          if (typeof overview.appid !== "number") {
-            logger.error(`Failed to patch ${route}, ret1#appid!`);
-            return ret1;
-          }
-
-          if (typeof overview.display_name !== "string") {
-            logger.error(`Failed to patch ${route}, ret1#display_name!`);
-            return ret1;
-          }
-
-          const { app_type: appType, appid: appId, display_name: appName } = overview;
-          if (moonDeckAppLauncher.moonDeckApp.value?.moonDeckAppId === appId) {
-            // We are suppressing the navigation to custom shortcut ONLY if this happens after
-            // launch via the moondeck button and we MUST navigate back ONLY once
-            if (moonDeckAppLauncher.moonDeckApp.canRedirect()) {
-              Navigation.NavigateBack();
+        const patchHandler = createReactTreePatcher([
+          (tree: any) => {
+            const children = findInReactTree(tree, (x: any) => x?.props?.children?.props?.overview)?.props?.children;
+            if (typeof children !== "object") {
+              logger.debug(`Failed to patch ${route}, @children!`);
+              return null;
             }
 
-            return ret1;
-          }
+            const overview = children.props?.overview;
+            if (typeof overview !== "object") {
+              logger.error(`Failed to patch ${route}, @overview!`);
+              return null;
+            }
 
-          wrapReactType(ret1Children);
-          afterPatch(
-            ret1Children.type,
-            "type",
-            (_2: Array<Record<string, unknown>>, ret2?: ReactElement): ReactElement | undefined => {
-              type ParentElement = ReactElement<{ children: Array<ReactElement<{ id?: string; overview?: unknown; onShowLaunchingDetails?: unknown }>>; className: string }>;
-              const parent = findInReactTree(ret2, (x: ParentElement) => Array.isArray(x?.props?.children) && x?.props?.className?.includes(appDetailsClasses.InnerContainer)) as ParentElement;
-              if (typeof parent !== "object") {
-                logger.error(`Failed to patch ${route} - parent element not found!`);
-                return ret2;
+            if (typeof overview.app_type !== "number") {
+              logger.error(`Failed to patch ${route}, @app_type!`);
+              return null;
+            }
+
+            if (typeof overview.appid !== "number") {
+              logger.error(`Failed to patch ${route}, @appid!`);
+              return null;
+            }
+
+            if (typeof overview.display_name !== "string") {
+              logger.error(`Failed to patch ${route}, @display_name!`);
+              return null;
+            }
+
+            ({ app_type: appType, appid: appId, display_name: appName } = overview);
+            if (moonDeckAppLauncher.moonDeckApp.value?.moonDeckAppId === appId) {
+              // We are suppressing the navigation to custom shortcut ONLY if this happens after
+              // launch via the moondeck button and we MUST navigate back ONLY once
+              if (moonDeckAppLauncher.moonDeckApp.canRedirect()) {
+                Navigation.NavigateBack();
               }
 
-              const hltbIndex = parent.props.children.findIndex((x) => x.props.id === "hltb-for-deck");
-              const appPanelIndex = parent.props.children.findIndex((x) => x.props.overview && x.props.onShowLaunchingDetails);
-              parent.props.children.splice(
-                hltbIndex < 0 ? (appPanelIndex < 0 ? -1 : appPanelIndex - 1) : hltbIndex,
-                0,
-                <MoonDeckLaunchButtonAnchor
-                  appId={appId}
-                  appName={appName}
-                  appType={appType}
-                  moonDeckAppLauncher={moonDeckAppLauncher}
-                  settingsManager={settingsManager} />
-              );
-
-              return ret2;
+              return null;
             }
+
+            return children;
+          }
+        ], (_2: Array<Record<string, unknown>>, ret?: ReactElement) => {
+          type ParentElement = ReactElement<{ children: Array<ReactElement<{ id?: string; overview?: unknown; onShowLaunchingDetails?: unknown }>>; className: string }>;
+          const parent = findInReactTree(ret, (x: ParentElement) => Array.isArray(x?.props?.children) && x?.props?.className?.includes(appDetailsClasses.InnerContainer)) as ParentElement;
+          if (typeof parent !== "object") {
+            logger.error(`Failed to patch ${route} - parent element not found!`);
+            return null;
+          }
+
+          if (typeof appType !== "number" || typeof appId !== "number" || typeof appName !== "string") {
+            logger.error(`Failed to patch ${route} - undefined data!`);
+            return null;
+          }
+
+          const hltbIndex = parent.props.children.findIndex((x) => x.props.id === "hltb-for-deck");
+          const appPanelIndex = parent.props.children.findIndex((x) => x.props.overview && x.props.onShowLaunchingDetails);
+          parent.props.children.splice(
+            hltbIndex < 0 ? (appPanelIndex < 0 ? -1 : appPanelIndex - 1) : hltbIndex,
+            0,
+            <MoonDeckLaunchButtonAnchor
+              appId={appId}
+              appName={appName}
+              appType={appType}
+              moonDeckAppLauncher={moonDeckAppLauncher}
+              settingsManager={settingsManager} />
           );
-          return ret1;
-        }
-      );
-      return routeProps;
+
+          return ret;
+        });
+
+        afterPatch(routeProps, "renderFunc", patchHandler);
+      }
+
+      return tree;
     }
   );
 }
