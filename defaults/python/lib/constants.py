bd0fd7e27a564fae4e1e69f09890add8fb5d33bd
FrogTheFrog <warliukz@gmail.com>
2025-02-16 16:26:39 +0200

    meh

--- defaults/python/lib/constants.py
+++ defaults/python/lib/constants.py
@@ -15,7 +15,7 @@ def get_user():
 
 CURRENT_USER = get_user()
 BUDDY_API_VERSION = 5
-CONFIG_VERSION_LITERAL = typing.Literal[25]
+CONFIG_VERSION_LITERAL = typing.Literal[26]
 CONFIG_DIR = str(pathlib.Path("/home", CURRENT_USER, ".config", "moondeck"))
 CONFIG_FILENAME = "settings.json"
 LOG_FILE = "/tmp/moondeck.log"
