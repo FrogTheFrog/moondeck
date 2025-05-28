b2261d45be5f58065b9d9b2a4a04cb4b96e3f827
FrogTheFrog <warliukz@gmail.com>
2025-05-28 22:33:34 +0300

    Add cmd handling template

--- defaults/python/cli.py
+++ defaults/python/cli.py
@@ -14,58 +14,105 @@ def add_plugin_to_path():
 add_plugin_to_path()
 
 from pathlib import Path
-from lib.runner.moondeckapprunner import MoonDeckAppRunner
-from lib.runner.moonlightonlyrunner import MoonlightOnlyRunner
-from lib.logger import logger, set_logger_settings, enable_debug_level
-from lib.utils import is_moondeck_runner_ready
-from lib.runner.settingsparser import parse_settings, RunnerType
+
+from lib.logger import logger, set_logger_settings
+from lib.cli.cmd.host.scan import execute as cmd_host_scan
 
 import argparse
-
 import sys
 import asyncio
-import lib.constants as constants
-import lib.runnerresult as runnerresult
 # autopep8: on
+
+
+class ArgumentParserWithRedirect(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("formatter_class",
+                          argparse.ArgumentDefaultsHelpFormatter)
+        super().__init__(*args, **kwargs)
+
+    def _print_message(self, message, file=None):
+        if message:
+            message = message.rstrip('\n')
+            if file == sys.stderr:
+                logger.error(message)
+            else:
+                logger.info(message)
 
 
 async def main():
     try:
-        # General arguments
-        parser = argparse.ArgumentParser(
+        # ---- Setup early logging to at least print something out
+        set_logger_settings(None, print_to_stdout=True)
+
+        # ---- General parser setup [start]
+        parser = ArgumentParserWithRedirect(
             description="MoonDeck CLI for interaction with MoonDeck Buddy and more.",
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        parser.add_argument("--verbose", action='store_true',
+            add_help=False)
+        parser.add_argument("--verbose", action="store_true",
                             help="enable verbose logging")
         parser.add_argument("--log-file", type=Path,
                             help="path to the optional log file")
-        # parser.add_argument("--config-file", type=Path,
-        #                     default=Path("config.json"), help="path to the config file")
-        parser_args, _ = parser.parse_known_args()
 
-        # Setup logging
+        # -------- Setup logging (as early as possible)
+        parser_args, _ = parser.parse_known_args()
         set_logger_settings(parser_args.log_file,
                             print_to_stdout=True,
                             log_preamble=f"Executing: {sys.argv[:]}, CWD: {Path.cwd()}",
                             verbose=parser_args.verbose)
 
-        logger.debug("LOL")
-        logger.info("LOL")
-        logger.warning("LOL")
-        logger.error("LOL")
-        logger.critical("LOL")
-        sys.exit(1)
+        # ---- General parser setup [finish]
+        parser = ArgumentParserWithRedirect(parents=[parser])
+        parser.add_argument("--config-file", type=Path,
+                            default=Path("config.json"), help="path to the config file")
 
-        # subparsers = parser.add_subparsers(dest="command", help="command to execute")
+        # ---- Setup subparser groups
+        group_subparsers = parser.add_subparsers(
+            dest="group", help="group of specialized commands")  # TODO: better wording
 
-        # # Scan command
-        # scan_parser = subparsers.add_parser("scan", help="scan for available hosts")
-        # scan_parser.add_argument("--timeout", type=float, default=5.0, help="Scan timeout in seconds")
+        # ---- Setup host group
+        host_parser = group_subparsers.add_parser(
+            "host", help="host related commands")
+        host_subparsers = host_parser.add_subparsers(
+            dest="command", help="command to execute")
 
-        # parser.print_help()
+        # -------- Setup scan command
+        scan_parser = host_subparsers.add_parser(
+            "scan", help="scan for available hosts")
+        scan_parser.add_argument(
+            "--timeout", type=float, default=5.0, help="Scan timeout in seconds")
+        scan_parser.add_argument(
+            "--dry", action="store_true", help="Do not save any changes")
+        scan_parser.add_argument("--overwrite", action="store_true",
+                                 help="Remove any hosts from the config that were not found during scan")
+
+        # ---- Parse all of the commands
+        parser_args, _ = parser.parse_known_args()  # Will exit if help is specified
+        parser_args = vars(parser_args)
+
+        # ---- Delegate the commands
+        if parser_args["group"] is None or parser_args.get("command", None) is None:
+            parser = ArgumentParserWithRedirect(
+                parents=[parser], add_help=False)
+            # Will exit since help is specified
+            parser.parse_known_args(sys.argv[1:] + ["--help"])
+
+        cmds = {
+            "host": {
+                "scan": cmd_host_scan
+            }
+        }
+
+        cmd = cmds.get(parser_args["group"], {}).get(
+            parser_args["command"], None)
+        if cmd is not None:
+            sys.exit(await cmd(**parser_args))
+
+        raise Exception(
+            f"Unhandled parser branch {parser_args["group"]}->{parser_args["command"]}")
 
     except Exception:
         logger.exception("Unhandled exception")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
---
+++ defaults/python/lib/cli/utils.py
@@ -0,0 +1,45 @@
+import inspect
+import functools
+
+
+def ignore_unmatched_kwargs(f):
+    """
+    Make function ignore unmatched kwargs.
+    If the function already has the catch all **kwargs, do nothing.
+
+    https://stackoverflow.com/a/63787701
+    and
+    https://stackoverflow.com/a/42769789
+    """
+    def contains_var_kwarg(f):
+        return any(
+            param.kind == inspect.Parameter.VAR_KEYWORD
+            for param in inspect.signature(f).parameters.values()
+        )
+
+    if contains_var_kwarg(f):
+        return f
+
+    def is_kwarg_of(key, f):
+        param = inspect.signature(f).parameters.get(key, None)
+        return param and (
+            param.kind is inspect.Parameter.KEYWORD_ONLY or
+            param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD
+        )
+
+    def filter_kwargs(**kwargs):
+        return {
+            key: value
+            for key, value in kwargs.items()
+            if is_kwarg_of(key, f)
+        }
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        return f(*args, **filter_kwargs(**kwargs))
+
+    @functools.wraps(f)
+    async def async_wrapper(*args, **kwargs):
+        return await f(*args, **filter_kwargs(**kwargs))
+
+    return async_wrapper if inspect.iscoroutinefunction(f) else wrapper
