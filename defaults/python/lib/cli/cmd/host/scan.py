60894d60e05c7acf5b685486340ebb1d49ac8431
FrogTheFrog <warliukz@gmail.com>
2025-06-01 12:07:47 +0300

    Wrap config file in settings manager

--- defaults/python/lib/cli/cmd/host/scan.py
+++ defaults/python/lib/cli/cmd/host/scan.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from lib.cli.utils import cmd_entry
 from lib.hostinfo import scan_for_hosts
+from lib.cli.settings import CmdSettingsManager
 
 
 @cmd_entry
-async def execute(config_file: Path, timeout: float, dry: bool, overwrite: bool) -> int:
+async def execute(settings: CmdSettingsManager, timeout: float, dry: bool, overwrite: bool) -> int:
     hosts = await scan_for_hosts(timeout=timeout)
     print(hosts)
     return 0
