14667497ab4af4af575162f15a48977207dc928f
FrogTheFrog <warliukz@gmail.com>
2025-06-07 00:16:01 +0300

    Abort pairing on interrupt

--- defaults/python/lib/cli/cmd/host/pair.py
+++ defaults/python/lib/cli/cmd/host/pair.py
@@ -12,34 +12,43 @@ from lib.buddyclient import BuddyClient, BuddyException, HelloResult, PairingRes
 @cmd_entry
 async def execute(buddy_client: BuddyClient, settings: CliSettings, host_id: str, buddy_port: int, pin: int):
     try:
-        await buddy_client.start_pairing(pin)
-        logger.info(f"Enter pin {pin} on host in Buddy's pop window to complete pairing.")
-    except BuddyException as err:
-        if err.result != PairingResult.AlreadyPaired:
-            logger.error(f"Could not start pairing: {err}")
-            return 1
-        else:
-            logger.info("Client is already paired - updating Buddy port only.")
-    
-    if not buddy_client.hello_was_ok:
-        logger.info("Waiting for pairing to finish...")
-        while True:
-            try:
-                await buddy_client.say_hello()
-                logger.info("Pairing has been completed.")
-                break
-            except BuddyException as err:
-                if err.result == HelloResult.Pairing:
-                    await asyncio.sleep(1)
-                    continue
-
-                if err.result == HelloResult.NotPaired:
-                    logger.error("Pairing was aborted or timed out!")
-                    return 1
-                
-                logger.error(f"Could not finish pairing: {err}")
+        try:
+            await buddy_client.start_pairing(pin)
+            logger.info(f"Enter pin {pin} on host in Buddy's pop window to complete pairing.")
+        except BuddyException as err:
+            if err.result != PairingResult.AlreadyPaired:
+                logger.error(f"Could not start pairing: {err}")
                 return 1
+            else:
+                logger.info("Client is already paired - updating Buddy port only.")
+        
+        if not buddy_client.hello_was_ok:
+            logger.info("Waiting for pairing to finish...")
+            while True:
+                try:
+                    await buddy_client.say_hello()
+                    logger.info("Pairing has been completed.")
+                    break
+                except BuddyException as err:
+                    if err.result == HelloResult.Pairing:
+                        await asyncio.sleep(1)
+                        continue
 
-    # This is the only place where this can be updated
-    settings["hosts"][host_id]["buddyPort"] = buddy_port
-    return 0
+                    if err.result == HelloResult.NotPaired:
+                        logger.error("Pairing was aborted or timed out!")
+                        return 1
+                    
+                    logger.error(f"Could not finish pairing: {err}")
+                    return 1
+
+        # This is the only place where this can be updated
+        settings["hosts"][host_id]["buddyPort"] = buddy_port
+        return 0
+    except asyncio.CancelledError as err:
+        try:
+            logger.info("Aborting pairing...")
+            await buddy_client.abort_pairing()
+        except Exception as abort_err:
+            logger.debug(f"Exception while aborting pairing: {abort_err}")
+
+        raise err
