7d5e152dd68d9b319e1d6353ba6b7903b94c08d6
FrogTheFrog <warliukz@gmail.com>
2025-06-08 18:16:51 +0300

    Add "host restart" cmd

---
+++ defaults/python/lib/cli/cmd/host/restart.py
@@ -0,0 +1,18 @@
+from lib.cli.utils import buddy_session, cmd_entry, host_pattern_matcher, settings_watcher
+from lib.buddyclient import BuddyClient, PcStateChange, BuddyException, HelloResult
+from lib.logger import logger
+
+
+@settings_watcher()
+@host_pattern_matcher(match_one=True)
+@buddy_session()
+@cmd_entry
+async def execute(buddy_client: BuddyClient):
+    try:
+        await buddy_client.change_pc_state(PcStateChange.Restart)
+    except BuddyException as err:
+        if err.result != HelloResult.Restarting:
+            raise err
+
+    logger.info("Host is restarting.")
+    return 0
